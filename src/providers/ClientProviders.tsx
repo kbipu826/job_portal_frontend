653c86de16b9ec04059d1cf4dfbb47e21d7bfb78
treeinfra3103 <t2854340@gmail.com>
2025-05-27 03:33:55 +0530

    Initial commit231

--- src/providers/ClientProviders.tsx
+++ src/providers/ClientProviders.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 import { Toaster } from 'react-hot-toast';
@@ -8,6 +8,16 @@ import LoadingProvider from './LoadingProvider';
 import ScriptProvider from './ScriptProvider';
 
 const ClientProviders = ({ children }: { children: React.ReactNode }) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return <>{children}</>;
+  }
+
   return (
     <SessionProvider>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
