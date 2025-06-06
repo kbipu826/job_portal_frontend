22d2e24b651e7be9ebe60095bde90617b24430cc
treeinfra3103 <t2854340@gmail.com>
2025-06-06 23:10:19 +0530

    post a job frontendend22

--- src/components/EmployeeSidebar/EmployeeLayout.tsx
+++ src/components/EmployeeSidebar/EmployeeLayout.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
 import { signOut } from 'next-auth/react';
 import EmployeeSidebar from './EmployeeSidebar';
+import EmployeeHeader from './EmployeeHeader';
 import '@/styles/EmployeeSidebar.css';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 
@@ -57,104 +58,11 @@ const EmployeeLayout = ({ children }: EmployeeLayoutProps) => {
         onMobileToggle={toggleMobileMenu}
       />
 
-      <div id="header-admin-wrap" className={isSidebarCollapsed ? 'sidebar-collapsed' : ''}>
-        <div id="header-admin">
-          <div className="container">
-            <div className="header-left">
-              {!isMobile && (
-                <button 
-                  className="sidebar-collapse-btn" 
-                  onClick={toggleSidebar}
-                  aria-label={isSidebarCollapsed ? "Expand sidebar" : "Collapse sidebar"}
-                >
-                  {isSidebarCollapsed ? <FiChevronRight size={18} /> : <FiChevronLeft size={18} />}
-                </button>
-              )}
-            </div>
-            <div className="header-right">
-              <ul className="header-widget-wrap">
-                <li className="header-widget dashboard-message-dropdown">
-                  <div className="dropdown">
-                    <a href="javascript:;" className="dropdown-toggle jobzilla-admin-messange" id="ID-MSG_dropdown" data-bs-toggle="dropdown">
-                      <i className="far fa-envelope"></i>
-                      <span className="notification-animate">4</span>
-                    </a>
-                    <div className="dropdown-menu" aria-labelledby="ID-MSG_dropdown">
-                      <div className="message-list dashboard-widget-scroll">
-                        <ul>
-                          <li className="clearfix">
-                            <span className="msg-avtar">
-                              <img src="/images/user-avtar/pic1.jpg" alt="" />
-                            </span>
-                            <div className="msg-texting">
-                              <strong>Alexa Johnson</strong>
-                              <small className="msg-time">
-                                <span className="far fa-clock p-r-5"></span>12 mins ago
-                              </small>
-                              <p>Lorem ipsum dolor sit amet, consectetur...</p>
-                            </div>
-                          </li>
-                        </ul>
-                        <div className="message-view-all">
-                          <a href="javascript:;">View All</a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-                <li className="header-widget dashboard-noti-dropdown">
-                  <div className="dropdown">
-                    <a href="javascript:;" className="dropdown-toggle jobzilla-admin-notification" id="ID-NOTI_dropdown" data-bs-toggle="dropdown">
-                      <i className="far fa-bell"></i>
-                      <span className="notification-animate">8</span>
-                    </a>
-                    <div className="dropdown-menu" aria-labelledby="ID-NOTI_dropdown">
-                      <div className="dashboard-widgets-header">You have 7 notifications</div>
-                      <div className="noti-list dashboard-widget-scroll">
-                        <ul>
-                          <li>
-                            <a href="#">
-                              <span className="noti-icon"><i className="far fa-bell"></i></span>
-                              <span className="noti-texting">Devid applied for <b>Webdesigner.</b> </span> 
-                            </a>
-                          </li>
-                        </ul>
-                        <div className="noti-view-all">
-                          <a href="javascript:;">View All</a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-                <li className="header-widget">
-                  <div className="dashboard-user-section">
-                    <div className="listing-user">
-                      <div className="dropdown">
-                        <a href="javascript:;" className="dropdown-toggle" id="ID-ACCOUNT_dropdown" data-bs-toggle="dropdown">
-                          <div className="user-name text-black">
-                            <span>
-                              <img src="/images/user-avtar/pic4.jpg" alt="" />
-                            </span>
-                            {session?.user?.name || 'User'}
-                          </div>
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="ID-ACCOUNT_dropdown">
-                          <ul>
-                            <li><a href="/employee-dashboard"><i className="fa fa-home"></i>Dashboard</a></li>
-                            <li><a href="/dash-messages"><i className="fa fa-envelope"></i> Messages</a></li>
-                            <li><a href="/dash-my-profile"><i className="fa fa-user"></i> Profile</a></li>
-                            <li><a href="#" onClick={handleLogout}><i className="fa fa-share-square"></i> Logout</a></li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
+      <EmployeeHeader 
+        isSidebarCollapsed={isSidebarCollapsed}
+        onToggleSidebar={toggleSidebar}
+        isMobile={isMobile}
+      />
 
       <div id="content" className={isSidebarCollapsed ? 'sidebar-collapsed' : ''}>
         {children}
